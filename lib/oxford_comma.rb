bff6a755112731d96bb6080b1a3df5a0e9d3bc76
Micah Bowie <micahbowie20@gmail.com>
2020-06-27 16:53:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+  
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(' and ')
+  elsif array.length == 3
+    last = array.pop.prepend(', and ')
+    return array.join(', ')<< last
+  else 
+    last = array.pop.prepend(', and ')
+    return array.join(', ')<< last
+  end
 end
